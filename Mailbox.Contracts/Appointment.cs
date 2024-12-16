c047c35db3ab3c99be27b5fe7fbbff359b4702d1
TonyStoynev <toni.stoinev@gmail.com>
2024-12-16 08:27:53 +0200

    Add `Mailbox.Contracts` project containing the core models for the project `Email` and `Appointment`

---
+++ Mailbox.Contracts/Appointment.cs
@@ -0,0 +1,4 @@
+﻿namespace Mailbox.Contracts;
+
+[GenerateSerializer, Alias(nameof(Appointment))]
+public record Appointment(string EventName, string Organizer, List<string> Participants, DateTime StartTime, DateTime EndTime);
---
+++ Mailbox.Contracts/Email.cs
@@ -0,0 +1,4 @@
+﻿namespace Mailbox.Contracts;
+
+[GenerateSerializer, Alias(nameof(Email))]
+public sealed record Email(Guid Id, string To, string Subject, string Body);
